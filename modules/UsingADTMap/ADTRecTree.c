f03d6ed8b94d18f197f4f372373e3212db7bd525
nikstam99 <nikstam99@gmail.com>
2023-05-25 20:18:42 +0300

    project2

--- modules/UsingADTMap/ADTRecTree.c
+++ modules/UsingADTMap/ADTRecTree.c
@@ -8,7 +8,11 @@
 #include "ADTRecTree.h"
 #include "ADTMap.h"
 
+// Counter που μετράει το σύνολο των δέντρων που έχουν δημιουργηθεί
 Pointer count = (Pointer)0;
+
+// Global maps στα οποία αναθέτουμε το περιεχόμενο και το μέγεθος ενός δέντρου,
+// καθώς και το δεξί και το αριστερό υποδέντρο του (αν υπάρχουν)
 Map map_val;
 Map map_Size;
 Map map_left;
@@ -22,7 +26,10 @@ int compare_keys(Pointer a, Pointer b) {
 
 
 RecTree rectree_create(Pointer value, RecTree left, RecTree right) {
+	// Το δέντρο παίρνει την τιμή του global counter
 	Pointer tree = count;
+
+	// Στην πρώτη φορά που καλείται η create δημιουργούμε τα global maps
 	if (count == (Pointer)0) {
 		map_val = map_create((CompareFunc)compare_keys, NULL, NULL);
 		map_Size = map_create((CompareFunc)compare_keys, NULL, NULL);
@@ -35,29 +42,43 @@ RecTree rectree_create(Pointer value, RecTree left, RecTree right) {
 	if (value != NULL) {
 		count++;
 		tree++;
+
+		// Αν το δέντρο δεν είναι κενό αυξάνουμε τον counter και τοποθετούμε το 
+		// value στο κατάλληλο map
 		map_insert(map_val, tree, value);
 		s = 1;
 	}
+	// Αν το δέντρο είναι κενό τοποθετούμε το 0 στο map
 	else map_insert(map_val, REC_TREE_EMPTY, REC_TREE_EMPTY);
 	
+	//Αν το αριστερό υποδέντρο δεν είναι NULL το τοποθετούμε στο map_left
+	// και αυξάνουμε το size του δέντρου
 	if (left != NULL) {
 		map_insert(map_left, tree, left);
 		size_left = map_find(map_Size, left) - (Pointer)0;
 		s += size_left;
 	}
 
+	//Αν το δεξί υποδέντρο δεν είναι NULL το τοποθετούμε στο map_right
+	// και αυξάνουμε το size του δέντρου
 	if (right != NULL) {
 		map_insert(map_right, tree, right);
 		size_right = map_find(map_Size, left) - (Pointer)0;
 		s += size_right;
 	}
+	// Κάνουμε cast το size σε pointer γιατί το map δέχεται pointers 
+	// και το τοποθετούμε στο map_size
 	Pointer size = s + (Pointer)0;
 	map_insert(map_Size, tree, size);
+
+	// Επιστρέφουμε το casted σε RecTree δέντρο
 	return (RecTree)tree;
 }
 
 // Επιστρέφει τον αριθμό στοιχείων που περιέχει το δέντρο.
 int rectree_size(RecTree tree) {
+
+	// Αν το δέντρο δεν είναι κενό, το βρίσκουμε στο map_size και επιστρέφουμε το size του
 	if (tree != REC_TREE_EMPTY) {
 		Pointer size = map_find(map_Size, tree);
 		return size - (Pointer)0;
@@ -67,22 +88,28 @@ int rectree_size(RecTree tree) {
 
 // Ελευθερώνει όλη τη μνήμη που δεσμεύει το δέντρο tree.
 void rectree_destroy(RecTree tree) {
+
+	// Αφαιρούμε το δέντρο που θέλουμε να καταστρέψουμε απο τα maps
 	if (map_size(map_val)) {
 		map_remove(map_Size, tree);
 		map_remove(map_val, tree);
 		map_remove(map_left, tree);
 		map_remove(map_right, tree);
 	}
+	// Αν το map_size είναι άδειο καταστρέφουμε τα maps
 	if (!map_size(map_val)){
 		map_destroy(map_Size);
 		map_destroy(map_val);
 		map_destroy(map_left);
 		map_destroy(map_right);
+
+		// Επαναφέρουμε τον pointer σε 0
 		count = (Pointer)0;
 	}
 }
 
-// Επιστρέφουν την τιμή (στη ρίζα), το αριστερό και το δεξί υποδέντρο του δέντρου tree.
+// Επιστρέφουν την τιμή (στη ρίζα), το αριστερό και το δεξί υποδέντρο του δέντρου tree
+// ψάχνοντας το δέντρο στα κατάλληλα maps
 Pointer rectree_value(RecTree tree) {
 	return map_find(map_val, tree);
 }
--- modules/UsingLinkedTree/ADTRecTree.c
+++ modules/UsingLinkedTree/ADTRecTree.c
@@ -23,15 +23,21 @@ struct rec_tree {
 // Δημιουργεί και επιστρέφει ένα νέο δέντρο, με τιμή (στη ρίζα) value και υποδέντρα left και right.
 
 RecTree rectree_create(Pointer value, RecTree left, RecTree right) {
-	RecTree tree = malloc (sizeof (*tree));
+	// Δέσμευση μνήμης για το δέντρο
+	RecTree tree = malloc (sizeof (*tree)); 
+
+	// Ανάθεση του value
 	tree->value = value;
+	// Αρχικοποίηση του size σε 1
 	tree->size = 1;
 	if (left != NULL) {
 	tree->left = left;
+	// Προσθέτουμε στο size το size του αριστερού υποδέντρου
 	tree->size += left->size;
 	}
 	else tree->left = NULL;
 	if (right != NULL) {
+	// Προσθέτουμε στο size το size του δεξιού υποδέντρου
 	tree->right = right;
 	tree->size += right->size;
 	}
